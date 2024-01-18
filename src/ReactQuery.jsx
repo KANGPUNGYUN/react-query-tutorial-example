1e3f15c92eddd9ae3c963429426d9966f590d54b
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-01-19 00:07:10 +0900

    learn about Optimistic Updates

--- src/ReactQuery.jsx
+++ src/ReactQuery.jsx
@@ -1,7 +1,19 @@
-import { useProductName } from "./hooks/useProductName";
+import { useProductName, useAddProduct } from "./hooks/useProductName";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function ReactQuery() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const { mutate: addProduct } = useAddProduct();
+
+  const handleCreate = () => {
+    console.log({ name, price });
+    const data = { name, price };
+    addProduct(data);
+  };
+
   const onSuccess = (data) => {
     console.log("데이터 가져오기 후 사이드 이펙트 수행", data);
   };
@@ -23,7 +35,23 @@ export function ReactQuery() {
   return (
     <>
       <div className="text-4xl">ReactQuery</div>
-
+      <div className="space-x-2">
+        <input
+          className="border"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className="border"
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
+        />
+        <button className="border" onClick={handleCreate}>
+          Create
+        </button>
+      </div>
       <ul className="list-disc p-4">
         {data &&
           data.data?.map((product) => (
--- src/hooks/useProductName.js
+++ src/hooks/useProductName.js
@@ -1,8 +1,13 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
+import { v4 as uuid } from "uuid";
 
 function fetchProducts() {
   return axios.get("http://localhost:3000/products");
+}
+
+function addProduct(product) {
+  return axios.post("http://localhost:3000/products", product);
 }
 
 export function useProductName(onSuccess, onError) {
@@ -15,5 +20,49 @@ export function useProductName(onSuccess, onError) {
     //     .map((p) => p.name);
     //   return productName;
     // },
+  });
+}
+
+export function useAddProduct() {
+  const queryClient = useQueryClient();
+  return useMutation(addProduct, {
+    /*
+    onSuccess: (data) => {
+      // 1) invalidateQueries 함수는 queryKey의 쿼리를 즉시 무효화 시키고 다시 fetching 시켜서, POST 액션 후에 잠깐의 시간 지체없이 바로 화면에 업데이트된 정보가 보여짐.
+      // queryClient.invalidateQueries("get-product");
+
+      // 2) POST 이후 다시 GET 액션이 되는 불필요한 작업을 줄이기 위해 아래의 코드를 사용한다.
+      queryClient.setQueryData("get-product", (oldProductData) => {
+        return {
+          ...oldProductData,
+          data: [...oldProductData.data, data.data],
+        };
+      });
+    },
+    */
+
+    // Optimistic Updates: 요청을 보내는 것과 동시에 결과를 예측하여 예측한 결과를 UI에 반영하는 가장 이상적인 업데이트 방법
+    // 참고 사이트: https://mycodings.fly.dev/blog/2023-10-02-react-query-howto-usemutation-in-depth
+    onMutate: async (newProduct) => {
+      await queryClient.cancelQueries("get-product");
+      const previousProductData = queryClient.getQueryData("get-product");
+      queryClient.setQueryData("get-product", (oldProductData) => {
+        return {
+          ...oldProductData,
+          data: [...oldProductData.data, { id: uuid(), ...newProduct }],
+        };
+      });
+      return {
+        // previousProductData는 실제 POST 액션이 에러가 나서 취소가 됐을 때 원상복구 용도이다.
+        previousProductData,
+      };
+    },
+    onError: (_error, _product, context) => {
+      queryClient.setQueryData("get-product", context.previousProductData);
+    },
+    // onSettled은 POST 액션이 에러가 났거나 아니면 성공했을 때 실행되는 항목
+    onSettled: () => {
+      queryClient.invalidateQueries("get-product");
+    },
   });
 }
